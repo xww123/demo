12fcd97615b5b43b672806d01c5b0233333fad15
xww123 <unix_pro@126.com>
2017-10-10 00:05:05 +0800

    创建测试控制器

---
+++ web/controller/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: xuke
+ * Date: 2017/10/10
+ * Time: 0:03
+ */
+namespace web\controller;
+
+class index
+{
+    public function show(){
+        echo "youzan...";
+    }
+}
